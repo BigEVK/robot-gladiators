82d48256fbebbc50547b747223edc670f29d8e21
EVK <eric@ericvankirk.com>
2022-06-08 18:26:33 -0500

    arrays

--- assets/js/game.js
+++ assets/js/game.js
@@ -7,14 +7,24 @@ var playerAttack = 10;
 var playerMoney = 10;
 
 // You can also log multiple vlaues at once like this
-console.log(playerName, playerAttack, playerHealth);
+// console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
+// console.log(enemyNames);
+// console.log(enemyNames[0]);
+// console.log(enemyNames[1]);
+// console.log(enemyNames[2]);
+// console.log(enemyNames.length);
+// for(var i = 0; i < enemyNames.length; i++) {
+//     console.log(enemyNames[i]);
+//     console.log(i);
+//     console.log(enemyNames[i] + " is at " + " index");
+// }
 var enemyHealth = 50;
 var enemyAttack = 12;
 
 // fight function
-var fight = function () {
+var fight = function (enemyName) {
 
     // Alert players that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
@@ -62,8 +72,17 @@ var fight = function () {
         else {
             fight();
         }
+    } else {
+            window.alert("You need to choose a valid option. Try again!"); 
     }
 }
+// fight();
+
+
+
+
+
+
 // window.alert(playerName + " has chosen to skip the fight!");
 // } else {
 //     window.alert("You need to choose a valid option. Try again!");
@@ -101,4 +120,3 @@ var fight = function () {
 // };
 
 
-fight();
